ad48f04690b965ec296d02d377c65619e5084a95
Harsh Rajvanshi <harsh.rajvanshi1@gmail.com>
2022-05-05 17:56:51 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,79 +1,63 @@
-import React, { useState } from 'react'
-import { Link } from 'react-scroll'
+import { useState } from 'react'
 import './NavbarStyles.css'
-import Popup from
+import { Drawer, Box, Typography, IconButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
-    function Navbar() {
-        const [nav, setNav] = useState(false)
-        const handleNav = () => setNav(!nav)
-
+//install mui components first for this file to run 
+function Navbar() {
+    function MDrawer() {
+        const [isDrawerOpen, setIsDrawerOpen] = useState(false)
         return (
-            <div name='home' className={nav ? 'navbar navbar-bg' : 'navbar'}>
-                <div className={nav ? 'logo dark' : 'logo'}>
-                    <h2>King Flyer.</h2>
-                </div>
-                <ul className="nav-menu">
-                    <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
-                    <Link to='destinations' smooth={true} duration={500} ><li>Destinations</li></Link>
-                    <Link to='carousel' smooth={true} duration={500} ><li>Travel</li></Link>
-                    <button className="btn" onClick="Popup"> Login/Sign-up </button>
-                    <Popup />
-                </ul>
+            <>
+                <IconButton size='large' edge='start' color='inherit' aria-label='logo' onClick={() => setIsDrawerOpen(true)}>
+                    <MenuIcon />
+                </IconButton>
+                <Box>
+                <Drawer anchor="right" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+                    <Box p={2} width='250px' height='50px' textAlign='center' role='presentation'>
+                        <Typography variant='h6' component='div'>
+                            side panel
+                        </Typography>
 
+                    </Box>
+                </Drawer>
+                </Box>
+            </>
 
-
-
-                <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
-                    <ul className="mobile-nav">
-                        <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
-                        <Link to='destinations' smooth={true} duration={500} ><li>Destinations</li></Link>
-                        <Link to='carousel' smooth={true} duration={500} ><li>Travel</li></Link>
-                        <button className="btn" onClick={() => this.openModal()}>Open Modal</button>
-
-                    </ul>
-
-                </div>
-
-
-
-
-
-
-
-            </div>
         )
     }
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
 
-
-class Sample extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showModal: false,
-            loading: false,
-            error: null
-        };
-    }
-
-
-
-    openModal() {
-        this.setState({
-            showModal: true
-        });
-    }
-
-    closeModal() {
-        this.setState({
-            showModal: false,
-            error: null
-        });
-    }
+    return (
+        <div name='home' className={nav ? 'navbar navbar-bg' : 'navbar'}>
+            <div className={nav ? 'logo dark' : 'logo'}>
+                <h2>King Flyer</h2>
+            </div>
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>About</li>
+                <li>FAQ</li>
+                <li><MDrawer /></li>
+            </ul>
 
 
 
+
+            <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+                <ul className="mobile-nav">
+                    <li>Home</li>
+                    <li>Destinations</li>
+                    <li>Travel</li>
+
+
+                </ul>
+
+            </div>
+        </div>
+    )
 }
+
 
 
 export default Navbar
