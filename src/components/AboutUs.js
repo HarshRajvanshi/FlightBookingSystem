4587f5e1d47504251be88bca391b43a2045a5855
MonikaAgarwal542 <84187029+MonikaAgarwal542@users.noreply.github.com>
2022-05-07 06:07:03 +0530

    Add files via upload

---
+++ src/components/AboutUs.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar'
+import React from 'react'
+import './NavbarStyles.css'
+
+export default function AboutUs() {
+  return (
+    <>
+    
+     <Navbar/>
+    <div>
+      
+      <h1>bdcbdnvbn</h1>
+    </div>
+    </>
+  )
+}
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,10 @@
+import { red } from '@mui/material/colors'
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <div>
+     <h2 style={{color:'red'}}> 404 Error:Page does not exist!</h2>
+    </div>
+  )
+}
