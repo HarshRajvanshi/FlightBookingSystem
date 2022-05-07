1db25e702a76280b7db747cb76c3b4d70c2f9b1a
MonikaAgarwal542 <84187029+MonikaAgarwal542@users.noreply.github.com>
2022-05-07 17:09:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 import Home from './Components/Home';
 import AboutUs from './Components/AboutUs'
 import Faq from './Components/Faq'
-import {Route,Routes}  from 'react-router-dom';
+import ErrorPage from './Components/ErrorPage';
+import {Navigate, Route,Routes}  from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import SearchFlight from './Components/SearchFlight';
+import SignupForm from './Components/SignupForm';
 function App() {
   return (
-    <div>
+    <div> 
        
      
      <BrowserRouter>
      
           <Routes>
-                <Route exact path='/' element={<Home/>}/>    
+                <Route exact path='/' element={<Navigate to='/home'/>}></Route>    
+                <Route exact path='/home' element={<Home/>}/>
                 <Route exact path='/AboutUs' element={<AboutUs/>}/> 
                 <Route exact path='/FAQ' element={<Faq/>}/>
-               <Route exact  path='/searchflight'  element={<SearchFlight/>}  /> 
-                <Route path='/error' element={<ErrorPage/>}/>
+                <Route exact  path='/searchflight'  element={<SearchFlight/>}/> 
+                <Route  path='/signup' element={<SignupForm/>}/>
+                <Route  path='/error' element={<ErrorPage/>}/>
+                
                 <Route path='*' element={<ErrorPage/>}/>
                 
           </Routes>
@@ -33,4 +37,5 @@ function App() {
 }
 
 export default App;
+
 
