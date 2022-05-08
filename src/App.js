29986a68183c9047cbee3ad4536d4f918cdbab75
Manish Rawat <85739547+manish098rawat@users.noreply.github.com>
2022-05-08 13:35:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 
 import Home from './Components/Home';
-import AboutUs from './Components/AboutUs'
-import Faq from './Components/Faq'
+import Aboutus from './Components/Aboutus/aboutus'
+import Faqs from './Components/Faq/Faqs'
 import ErrorPage from './Components/ErrorPage';
 import {Navigate, Route,Routes}  from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -18,8 +18,8 @@ function App() {
           <Routes>
                 <Route exact path='/' element={<Navigate to='/home'/>}></Route>    
                 <Route exact path='/home' element={<Home/>}/>
-                <Route exact path='/AboutUs' element={<AboutUs/>}/> 
-                <Route exact path='/FAQ' element={<Faq/>}/>
+                <Route exact path='/AboutUs' element={<Aboutus/>}/> 
+                <Route exact path='/FAQ' element={<Faqs/>}/>
                 <Route exact  path='/searchflight'  element={<SearchFlight/>}/> 
                 <Route  path='/signup' element={<SignupForm/>}/>
                 <Route  path='/error' element={<ErrorPage/>}/>
