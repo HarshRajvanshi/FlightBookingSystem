462b74da065debb7746d8a2a4372d7258c650352
raghav gautam <85741127+raghav-gif@users.noreply.github.com>
2022-05-08 10:21:52 +0530

    Add files via upload

---
+++ src/components/AdminDashboard/Dashboard (1).js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+import AppBar from '../Appbar/AppBar'
+
+export default function Dashboard() {
+    return (
+        <>
+            <AppBar/>
+            
+        </>
+
+
+    )
+}
+
+
---
+++ src/components/Appbar/AppBar.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+
+const pages = ['Update Flight', 'Add Flight', 'All Flight'];
+
+const ResponsiveAppBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'Poppins',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        King Flyer
+                    </Typography>
+
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                href='/'
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default ResponsiveAppBar;
